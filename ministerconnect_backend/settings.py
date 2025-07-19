5ffbea694ca89194c2c55824b70d104f8ad0d61f
Gina Beth Russell <ginabethrussell@MacBookPro.cinci.rr.com>
2025-07-19 17:10:59 -0400

    more storage debugging

--- ministerconnect_backend/settings.py
+++ ministerconnect_backend/settings.py
@@ -10,7 +10,7 @@ env = environ.Env(DEBUG=(bool, False))
 environ.Env.read_env(os.path.join(BASE_DIR, ".env"))
 
 SECRET_KEY = env("SECRET_KEY")
-DEBUG = env("DEBUG")
+DEBUG = env.bool("DEBUG", default=False)
 
 ALLOWED_HOSTS = env.list("ALLOWED_HOSTS", default=["localhost", "127.0.0.1"])
 
